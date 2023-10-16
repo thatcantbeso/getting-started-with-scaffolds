39252ce9885b81d1ac781cdf04b5ac6d796b5f2e
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-16 17:01:46 +0000

    need to add movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
+  resources :books
+  # Routes for the Movie resource:
+
+  get("/movies/new", { :controller => "movies", :action => "new"})
+  get("/movies/:path_id/edit", { :controller => "movies", :action => "edit" })
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end
